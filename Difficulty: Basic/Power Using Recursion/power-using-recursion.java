ef411014ac31c349f3064de2c720aac10e0c1028
Imdad Taj <imdadtaj1@gmail.com>
2026-08-10 23:17:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Power Using Recursion/power-using-recursion.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int recursivePower(int n, int p) {
+        // code here
+        if(p==0) return 1;
+         int call = recursivePower(n,p/2);
+        if(p%2==0) return call*call;
+        else return n*call*call;
+    }
+}
