75afc49d3b5fe99bb9d7c0bc74c061a597e5c257
Imdad Taj <imdadtaj1@gmail.com>
2026-09-08 23:38:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First Node of Loop in Linked List/first-node-of-loop-in-linked-list.java
@@ -0,0 +1,38 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+};
+*/
+
+class Solution {
+    public int cycleStart(Node head) {
+        // code here
+        Node slow = head ; 
+        Node fast = head;
+        while ( fast != null && fast.next != null){
+              slow = slow.next;
+              fast = fast.next.next;
+              if(slow == fast )
+              break;
+              
+        }
+        if (fast == null || fast.next == null) {
+                   return -1;
+               }
+        
+        Node temp = head;
+        while(temp != slow){
+            slow =slow.next ;
+            temp = temp.next;
+        }
+        
+        return temp.data;
+    }
+}
