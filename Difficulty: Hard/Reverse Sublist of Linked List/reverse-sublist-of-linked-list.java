25110b4c47b4a4d6c99ebaaa3d01b2a0209d284f
Imdad Taj <imdadtaj1@gmail.com>
2026-09-13 22:01:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Reverse Sublist of Linked List/reverse-sublist-of-linked-list.java
+++ Difficulty: Hard/Reverse Sublist of Linked List/reverse-sublist-of-linked-list.java
@@ -11,27 +11,79 @@ class Node {
 */
 
 class Solution {
-    Node reverseBetween(int a, int b, Node head) {
-       ArrayList<Node> arr = new ArrayList<>();
-       Node temp = head;
-       while(temp!=null){
-           arr.add(temp);
-           temp=temp.next;
-       }
-       
-       int i = a-1, j =b-1;
-       while(i<j){
-           Node t1 = arr.get(i);
-           Node t2 =arr.get(j);
-           arr.set(i,t2);
-           arr.set(j,t1);
-           i++;
-           j--;
-       }
-       for(i = 0 ; i< arr.size() ; i++){
-           arr.get(i).next = (i == arr.size()-1) ? null : arr.get(i+1);
-       }
-       return arr.get(0);
-        
+   public static Node reverse(Node head){
+        Node curr = head;
+        Node prev = null;
+        Node fwd = null;
+        while(curr!=null){
+            fwd = curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=fwd;
+        }
+        return prev;
     }
+     Node reverseBetween(int l, int r, Node head) {
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        dummy.next =head;
+        
+        for(int i =1 ; i<=l-1;i++){
+            temp= temp.next;
+        }
+        Node tail1 = temp;
+        Node head2 = tail1.next;
+        
+        
+        for(int i =1;i<=r-l+1;i++){
+            temp =temp.next;
+        }
+        Node tail2 = temp;
+        Node head3 = tail2.next;
+        tail1.next= null;
+        tail2.next = null;
+        
+        reverse(head2);
+        
+        tail1.next = tail2;
+        head2.next=head3;
+        return dummy.next;
+        
+         
+     }
+    
 }
+
+
+
+
+
+
+
+// by ArrayList
+
+// class Solution {
+//     Node reverseBetween(int a, int b, Node head) {
+//       ArrayList<Node> arr = new ArrayList<>();
+//       Node temp = head;
+//       while(temp!=null){
+//           arr.add(temp);
+//           temp=temp.next;
+//       }
+       
+//       int i = a-1, j =b-1;
+//       while(i<j){
+//           Node t1 = arr.get(i);
+//           Node t2 =arr.get(j);
+//           arr.set(i,t2);
+//           arr.set(j,t1);
+//           i++;
+//           j--;
+//       }
+//       for(i = 0 ; i< arr.size() ; i++){
+//           arr.get(i).next = (i == arr.size()-1) ? null : arr.get(i+1);
+//       }
+//       return arr.get(0);
+        
+//     }
+// }
